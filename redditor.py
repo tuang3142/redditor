794490d47706d415dea6730535f2c6055005424d
Tuan Ng <tuan.nguyenviet271@gmail.com>
2018-11-27 16:19:26 +0900

    add data file, add subscribe command

--- redditor.py
+++ redditor.py
@@ -1,41 +1,41 @@
 import click
 import requests
 
+class colors:
+    HEADER = '\033[93m' + '\033[1m'
+    TITLE = '\033[92m'
+    END = '\033[0m'
+    WARNING = '\033[93m'
+
 @click.group()
 def main():
     """
-    Reddit API abuser, mostly from /r/dota2
+    CLI reddit for breakfast 
     """
     pass
 
+@main.command()
+def feed(subs):
+    for sub in subs:
+        url = 'https://www.reddit.com/r/{}/hot/.json'.format(sub)
+        params = { 'limit':5 }
+        response = requests.get(url, params=params)
+        try:
+            items = response.json()['data']['children'] 
+            threads = []
+            for item in items:
+                threads.append({
+                    'title': item['data']['title'],
+                    'score': item['data']['score'],
+                    'link': 'http://reddit.com' + item['data']['permalink']
+                })
+            print(colors.HEADER + '/r/{}'.format(sub) + colors.END)
+            for thread in threads:
+                title = colors.TITLE + '[{}] {}'.format(thread['score'], thread['title']) + colors.END
+                link = thread['link']
+                print(title, '\n', link)
+            print()
+        except:
+            print(colors.WARNING + 'too much reddit, back to work!' + colors.END)
 
 @main.command()
-@click.argument('subreddit')
-@click.option('--limit', '-l', help='number of threads to show')
-def hot(subreddit, limit=10):
-    """
-    Collect hot threads
-    """
-    url = 'https://www.reddit.com/r/{}/top/.json'.format(subreddit)
-    params = {
-        'limit':limit
-    }
-    response = requests.get(url, params=params)
-    try:
-        items = response.json()['data']['children'] 
-        threads = []
-        for item in items:
-            i = {
-                'title': item['data']['title'],
-                'score': item['data']['score'],
-                'link': 'reddit.com{}'.format(item['data']['permalink'])
-            }
-            threads.append(i)
-        for thread in threads:
-            print(thread)
-            print('\n')
-    except:
-        print('too much reddit, back to work')
-
-if __name__ == "__main__":
-    main()
