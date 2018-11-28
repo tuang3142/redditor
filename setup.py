4eb61a2f5ce8b4b44f50162b5b83820dee56299e
Tuan Ng <tuan.nguyenviet271@gmail.com>
2018-11-28 10:00:25 +0900

    update version

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
+
 setup(
     name='redditor',
-    version='0.1',
+    version='1.0',
     py_modules=['redditor'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        redditor=redditor:cli
+        redditor=redditor:main
     ''',
 )
